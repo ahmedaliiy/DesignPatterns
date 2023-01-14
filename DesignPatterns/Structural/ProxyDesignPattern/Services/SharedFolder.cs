605c53c0bf65ec5887ace33f352d4e17e92f7d38
Ahmed Ali <anour@youxel.com>
2023-01-15 01:46:43 +0200

    Implement Proxy Design Pattern

---
+++ DesignPatterns/Structural/ProxyDesignPattern/Services/SharedFolder.cs
@@ -0,0 +1,10 @@
+﻿using ProxyDesignPattern.Interfaces;
+
+namespace ProxyDesignPattern.Services
+{
+    public class SharedFolder : ISharedFolder
+    {
+        public void PerformRWOperations()
+            => Console.WriteLine("Performing Read Write operation on the Shared Folder");
+    }
+}
---
+++ DesignPatterns/Structural/ProxyDesignPattern/Services/SharedFolderProxy.cs
@@ -0,0 +1,26 @@
+﻿using ProxyDesignPattern.Classes;
+using ProxyDesignPattern.Interfaces;
+
+namespace ProxyDesignPattern.Services
+{
+    public class SharedFolderProxy : ISharedFolder
+    {
+        private ISharedFolder folder;
+        private Employee employee;
+        public SharedFolderProxy(Employee emp)
+        {
+            employee = emp;
+        }
+        public void PerformRWOperations()
+        {
+            if (employee.Role.ToUpper() == "CEO" || employee.Role.ToUpper() == "MANAGER")
+            {
+                folder = new SharedFolder();
+                Console.WriteLine("Shared Folder Proxy makes call to the RealFolder 'PerformRWOperations method'");
+                folder.PerformRWOperations();
+            }
+            else
+                Console.WriteLine("Shared Folder proxy says 'You don't have permission to access this folder'");
+        }
+    }
+}
