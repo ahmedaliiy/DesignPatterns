5fdcfb6783a20b670d7600f272135531d2e60004
Ahmed Ali <anour@youxel.com>
2023-01-15 01:30:54 +0200

    Implement Template Design Pattern

---
+++ DesignPatterns/Behavioral/TemplateDesignPattern/Classes/WoodenHouse.cs
@@ -0,0 +1,14 @@
+﻿namespace TemplateDesignPattern.Classes
+{
+    public class WoodenHouse : HouseTemplate
+    {
+        protected override void BuildFoundation()
+            => Console.WriteLine("Building foundation with cement, iron rods, wood and sand");
+        protected override void BuildPillars()
+            => Console.WriteLine("Building wood Pillars with wood coating");
+        protected override void BuildWalls()
+            => Console.WriteLine("Building Wood Walls");
+        protected override void BuildWindows()
+            => Console.WriteLine("Building Wood Windows");
+    }
+}
---
+++ DesignPatterns/Behavioral/TemplateDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using TemplateDesignPattern.Classes;
+
+Console.WriteLine("Build a Concrete House\n");
+HouseTemplate houseTemplate = new ConcreteHouse();
+// call the template method
+houseTemplate.BuildHouse();
+Console.WriteLine();
+Console.WriteLine("Build a Wooden House\n");
+houseTemplate = new WoodenHouse();
+// call the template method
+houseTemplate.BuildHouse();
+Console.Read();
